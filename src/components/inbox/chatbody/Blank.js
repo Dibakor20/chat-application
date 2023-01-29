a1e24a981e8cd80ad6263febe16965ca1f3dc6af
Debakor Acharjee <debakor10@gmail.com>
2023-01-29 23:11:49 +0600

    upsate

--- src/components/inbox/chatbody/Blank.js
+++ src/components/inbox/chatbody/Blank.js
@@ -4,6 +4,7 @@ export default function Blank() {
     return (
         <div className="relative w-full overflow-y-hiddeen h-[calc(100vh_-_64px)] flex flex-col items-center justify-center text-gray-700 p-5 space-y-5">
             <div>
+                {/* blank */}
                 <img src={blankImage} alt="Nothing here!" className="w-20" />
             </div>
             <div className="text-center">
